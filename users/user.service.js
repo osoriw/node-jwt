7dcfc9332bb3e0790d07cad67da9617293b6b661
Wilson Andrés Osorio Muñoz <wilsonandresosorio2018@gmail.com>
2021-09-26 18:08:08 -0500

    master: Proyecto NODe que sirve un API, para la autenticaciòn con JWT.

---
+++ users/user.service.js
@@ -0,0 +1,38 @@
+﻿const config = require('config.json');
+const jwt = require('jsonwebtoken');
+
+// users hardcoded for simplicity, store in a db for production applications
+const users = [{ id: 1, username: 'test', password: 'test', firstName: 'Test', lastName: 'User' },
+               { id: 1, username: 'wilson', password: '123', firstName: 'Wilson', lastName: 'Osorio' },
+               { id: 1, username: 'carlos', password: '123', firstName: 'Carlos', lastName: 'Bedoya' },
+               { id: 1, username: 'mario', password: '123', firstName: 'Mario', lastName: 'Lopez' }];
+
+module.exports = {
+    authenticate,
+    getAll
+};
+
+async function authenticate({ username, password }) {
+    const user = users.find(u => u.username === username && u.password === password);
+
+    if (!user) throw 'Username or password is incorrect';
+
+    // create a jwt token that is valid for 7 days
+    const token = jwt.sign({ sub: user.id }, config.secret, { expiresIn: '7d' });
+
+    return {
+        ...omitPassword(user),
+        token
+    };
+}
+
+async function getAll() {
+    return users.map(u => omitPassword(u));
+}
+
+// helper functions
+
+function omitPassword(user) {
+    const { password, ...userWithoutPassword } = user;
+    return userWithoutPassword;
+}
